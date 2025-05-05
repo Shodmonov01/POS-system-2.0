d2269697c1626fd44a4b76bef5bf93e435769d79
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-05-04 23:27:58 -0300

    refactor(branch): change name of buttons

--- src/components/cashiers/Columns.tsx
+++ src/components/cashiers/Columns.tsx
@@ -1,18 +1,20 @@
-import { ColumnDef } from '@tanstack/react-table'
-import { Cashier } from '@/types/api'
-import { Button } from '@/components/ui/button'
-import { Badge } from '@/components/ui/badge'
+import {ColumnDef} from '@tanstack/react-table';
+
+import {Button} from '@/components/ui/button';
+import {Badge} from '@/components/ui/badge';
+
+import {Cashier} from '@/types/api';
 
 export function getUserColumns(
     onEdit: (user: Cashier) => void,
     onDelete: (id: number) => void
 ): ColumnDef<Cashier>[] {
     return [
-        { accessorKey: 'name', header: 'Имя' },
+        {accessorKey: 'name', header: 'Имя'},
         {
             accessorKey: 'role',
             header: 'Роль',
-            cell: ({ row }) => (
+            cell: ({row}) => (
                 <Badge variant="outline">
                     {row.getValue('role') === 'admin' ? 'Администратор' : 'Кассир'}
                 </Badge>
@@ -21,15 +23,15 @@ export function getUserColumns(
         {
             id: 'actions',
             header: 'Действия',
-            cell: ({ row }) => {
+            cell: ({row}) => {
                 const usr = row.original
                 return (
                     <div className="flex space-x-2">
                         <Button size="sm" variant="outline" onClick={() => onEdit(usr)}>
-                            Edit
+                            Изменить
                         </Button>
                         <Button size="sm" variant="destructive" onClick={() => onDelete(usr.id)}>
-                            Delete
+                            Удалить
                         </Button>
                     </div>
                 )
