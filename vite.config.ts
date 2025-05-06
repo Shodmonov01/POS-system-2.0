c712e30e50222bbb5690c0c827d446a922fab9fe
Maksim <max_git@mail.ru>
2025-05-06 14:43:08 +0300

    change local port

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,32 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return {
+    plugins: [ react() ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+    optimizeDeps: {
+      exclude: [ 'lucide-react' ],
+    },
 
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://213.139.210.248:3000',
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, ''),
-  //     },
-  //   },
-  // },
+    server: {
+      port: process.env.VITE_SERVER_PORT ? parseInt(process.env.VITE_SERVER_PORT) : 5173,
+
+      // proxy: {
+      //   '/api': {
+      //     target: 'http://213.139.210.248:3000',
+      //     changeOrigin: true,
+      //     rewrite: (path) => path.replace(/^\/api/, ''),
+      //   },
+      // },
+    },
+  };
 });
